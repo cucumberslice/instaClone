746926ee3750b90baa7ace945ba38cb2b3484bcf
Yeshi Wangdi <ywblackberry@gmail.com>
2018-10-01 17:16:37 -0400

    created multiple views for image,border and separated the stylesheet outside component

--- src/InstaClone.js
+++ src/InstaClone.js
@@ -4,21 +4,57 @@ import { View, Text, StyleSheet, Image } from 'react-native'
 class InstaClone extends Component {
 
     render() {
-        return <View style={{ flex: 1, width: 100 + "%", height: 100 + "%"}} >
-         
-            {/* my remote pic format cannot be rendered at the moment */}
-            {/* <Image style={{ width: 100, height: 100 }} source={{ uri: "https://lh3.googleusercontent.com/yrYFTBh_KMfU54Le5ltfw4b4-FQ8MbFM3RZb_1jgsfJ2NAysXalvH4KKZHxuLPpbHY0CrC5Q4AIt5dqztIflCo8LaQ" }} /> */}
-            
-            {/* <View style={{ width: 100 + "%", height: 50}}/> */}
+        return <View style={{ flex: 1, width: 100 + "%", height: 100 + "%" }}>
+            <View style={styles.tempNav}>
+                <Text>Instagram</Text>
+            </View> 
+            <View style={styles.userBar}/>
 
-   
+            <View style={{flexDirection: "row"}}>
+               
+                <Image 
+                    style={{ width: 40, height: 40}}
+                    source={{
+                        uri: "https://www.google.com/search?q=funny+profile+pictures&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjSzsijkubdAhUmh-AKHYQtBu4Q_AUIDigB&biw=1920&bih=1001#imgrc=OggVpVKrYYsdjM:"
+            }}
+            />
+            <Text>NeoKun</Text>
+            </View>
+            <View>
+
+            </View>
+
+            {/* my remote pic format from turbo360 storage cannot be rendered at the moment */}
             
-            <Image 
-            source={{ 
-                uri: "https://www.petful.com/wp-content/uploads/2013/12/ear-mites-in-cats.png" }} 
-            style={{ width: 100+ "%", height: 50+ "%"}}/>
-          </View>;
-    }
+            <Image source={{ uri: 'https://facebook.github.io/react/logo-og.png' }}
+                style={{ width: 100 + "%", height: 100 }} />
+
+          </View>; 
+        }
+         
+            
 }
+
+const styles = StyleSheet.create({
+  tempNav: {
+    width: 100 + "%",
+    height: 56,
+    marginTop: 20,
+    backgroundColor: "rgb(250,250,250)",
+    borderBottomColor: "rgb(233,33,233)",
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    justifyContent: "center",
+    alignItems: "center"
+
+  },
+
+  userBar: {
+      width: 100 + "%",
+      height: 50,
+      backgroundColor: "rgb(255,255,255)",
+      flexDirection: "row"
+
+  }
+});
 
 export default InstaClone
