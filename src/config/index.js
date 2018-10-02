6b8d4d965d426f369db1e4b6884ea32862c9d42c
Yeshi Wangdi <ywblackberry@gmail.com>
2018-10-02 16:49:06 -0400

    added heart,bubbleChat and rightArrow icons in a new view

---
+++ src/config/index.js
@@ -0,0 +1,10 @@
+export default {
+    images: {
+        heartIcon: require('../../assets/heart.png'),
+        bubbleIcon: require('../../assets/message.png'),
+        arrowIcon: require('../../assets/rightArrow.png')
+    },
+    styleConstants : {
+        rowHeight: 50
+    }
+}
